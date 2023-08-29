8a0b44ad458e61b78eaea8361a5bfc1866e01d6f
Siyara Wijewardane <2021cs221@stu.ucsc.cmb.ac.lk>
2023-08-29 08:22:53 +0530

    second commit

--- 1.scala
+++ 1.scala
@@ -1,16 +1,19 @@
 object TemperatureConverter {
+  
   def calculateAverage(temperatures: List[Double]): Double = {
 
     val fahrenheitTemperatures = temperatures.map(celsius => (celsius * 9/5) + 32)
     val totalFahrenheit = fahrenheitTemperatures.reduce((total, temp) => total + temp)
     val averageFahrenheit = totalFahrenheit / temperatures.length
+
     averageFahrenheit
   }
 
   def main(args: Array[String]): Unit = {
     
-    val inputTemperatures = List(0, 10, 20, 30)
+    val inputTemperatures = List(0.0, 10.0, 20.0, 30.0) 
     val averageFahrenheit = calculateAverage(inputTemperatures)
-    println(s"Average Fahrenheit temperature: $averageFahrenheit")
+    println(s"Average Fahrenheit: $averageFahrenheit")
   }
 }
+
--- 2.scala
+++ 2.scala
@@ -2,16 +2,15 @@ object TotalLetters{
 
     def countLetterOccurrences(words: List[String]): Int = {
 
-        val lengths = words.map(_.length) // Transform words into their respective lengths
-        val totalLength = lengths.reduce(_ + _) // Calculate the total count of letter occurrences
+        val lengths = words.map(_.length) 
+        val totalLength = lengths.reduce(_ + _) 
         totalLength
     }
 
     def main(args: Array[String]): Unit = {
         
         val input = List("apple", "banana", "cherry", "date")
-        val output = countLetterOccurrences(input)
-        println(s"Total count of letter occurrences: $output")
+        println(s"Total count of letter occurrences: "+countLetterOccurrences(input))
     }
 
 }
